7ea829e1f6eee228768165b77a37349a4c7fd6a4
Siddarth <siddarthrgovindan@gmail.com>
2020-08-10 21:28:34 -0400

    setup camera and basic scene, created a T shape Tetromino block and block script, block script allows left/right movement of block

---
+++ Tetris/Assets/Scripts/Block.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        //Check for player inputs to move tetromino Left/Right
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right;
+        }
+    }
+}
