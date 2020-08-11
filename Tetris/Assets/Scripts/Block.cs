d61da06a6d14f59a93c0c2830aaedcf174138cd9
Siddarth <siddarthrgovindan@gmail.com>
2020-08-10 23:41:58 -0400

    blocks are now constrained to movement within the grid

--- Tetris/Assets/Scripts/Block.cs
+++ Tetris/Assets/Scripts/Block.cs
@@ -10,9 +10,13 @@ public class Block : MonoBehaviour
     //Private variables
     private float prevTime;
     
+    //References
+    private GridManager _gm;
+    
     void Start()
     {
-        
+        //Find the reference to the GridManager
+        _gm = GameObject.FindGameObjectWithTag("GridManager").GetComponent<GridManager>();
     }
     
     void Update()
@@ -21,10 +25,20 @@ public class Block : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += Vector3.left;
+            //Reverse the movement if it is not a valid move
+            if (!_gm.ValidMove(transform))
+            {
+                transform.position += Vector3.right;
+            }
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += Vector3.right;
+            //Reverse the movement if it is not a valid move
+            if (!_gm.ValidMove(transform))
+            {
+                transform.position += Vector3.left;
+            }
         }
 
         //Make the block fall based on the fall speed
@@ -32,6 +46,11 @@ public class Block : MonoBehaviour
         if (Time.time - prevTime >= (Input.GetKey(KeyCode.DownArrow)? fallSpeed/10 : fallSpeed))
         {
             transform.position += Vector3.down;
+            //Reverse the movement if it is not a valid move
+            if (!_gm.ValidMove(transform))
+            {
+                transform.position += Vector3.up;
+            }
             prevTime = Time.time;
         }
         
--- Tetris/Assets/Scripts/GridManager.cs
+++ Tetris/Assets/Scripts/GridManager.cs
@@ -1,17 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
+    //Public variables
     public int gridHeight = 20;
     public int gridLength = 10;
-
-    
     
     public bool ValidMove(Transform block)
     {
-        //Check is each square in the Tetromino block is moving to a valid location in the grid
+        //Check if each square in the Tetromino block is moving to a valid location in the grid
         foreach (Transform square in block)
         {
             //Get the x,y positions as int
@@ -19,7 +19,7 @@ public class GridManager : MonoBehaviour
             int yPos = Mathf.RoundToInt(square.transform.position.y);
             
             //Check if the position of the square is outside the grid - return false
-            if (xPos < 0 || xPos > gridLength || yPos < 0 || yPos > gridHeight)
+            if (xPos < 0 || xPos >= gridLength || yPos < 0 || yPos >= gridHeight)
             {
                 return false;
             }
